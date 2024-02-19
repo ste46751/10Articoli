95580f860b64a788d32472795bad565609291abd
ste46751 <stefanquarti060@gmail.com>
2024-02-19 09:33:33 +0100

    Aggiungere i file di progetto.

---
+++ 10Articoli/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace _10Articoli
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.Articolo = new System.Windows.Forms.Label();
+            this.btn_invia = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(110, 71);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(88, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(110, 97);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(88, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(110, 123);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(88, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // Articolo
+            // 
+            this.Articolo.AutoSize = true;
+            this.Articolo.Location = new System.Drawing.Point(107, 55);
+            this.Articolo.Name = "Articolo";
+            this.Articolo.Size = new System.Drawing.Size(45, 13);
+            this.Articolo.TabIndex = 3;
+            this.Articolo.Text = "Articolo:";
+            // 
+            // btn_invia
+            // 
+            this.btn_invia.Location = new System.Drawing.Point(110, 160);
+            this.btn_invia.Name = "btn_invia";
+            this.btn_invia.Size = new System.Drawing.Size(88, 33);
+            this.btn_invia.TabIndex = 4;
+            this.btn_invia.Text = "Invia";
+            this.btn_invia.UseVisualStyleBackColor = true;
+            this.btn_invia.Click += new System.EventHandler(this.btn_invia_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_invia);
+            this.Controls.Add(this.Articolo);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label Articolo;
+        private System.Windows.Forms.Button btn_invia;
+    }
+}
+
