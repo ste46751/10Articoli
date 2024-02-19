bc04362dab9486f9209b938355f5304493d35691
ste46751 <stefanquarti060@gmail.com>
2024-02-19 09:49:13 +0100

    parte iniziale

--- 10Articoli/Form1.Designer.cs
+++ 10Articoli/Form1.Designer.cs
@@ -28,33 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.txt_cod = new System.Windows.Forms.TextBox();
+            this.txt_descr = new System.Windows.Forms.TextBox();
+            this.txt_prezzo = new System.Windows.Forms.TextBox();
             this.Articolo = new System.Windows.Forms.Label();
             this.btn_invia = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // textBox1
+            // txt_cod
             // 
-            this.textBox1.Location = new System.Drawing.Point(110, 71);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(88, 20);
-            this.textBox1.TabIndex = 0;
+            this.txt_cod.Location = new System.Drawing.Point(110, 71);
+            this.txt_cod.Name = "txt_cod";
+            this.txt_cod.Size = new System.Drawing.Size(88, 20);
+            this.txt_cod.TabIndex = 0;
             // 
-            // textBox2
+            // txt_descr
             // 
-            this.textBox2.Location = new System.Drawing.Point(110, 97);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(88, 20);
-            this.textBox2.TabIndex = 1;
+            this.txt_descr.Location = new System.Drawing.Point(110, 97);
+            this.txt_descr.Name = "txt_descr";
+            this.txt_descr.Size = new System.Drawing.Size(88, 20);
+            this.txt_descr.TabIndex = 1;
             // 
-            // textBox3
+            // txt_prezzo
             // 
-            this.textBox3.Location = new System.Drawing.Point(110, 123);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(88, 20);
-            this.textBox3.TabIndex = 2;
+            this.txt_prezzo.Location = new System.Drawing.Point(110, 123);
+            this.txt_prezzo.Name = "txt_prezzo";
+            this.txt_prezzo.Size = new System.Drawing.Size(88, 20);
+            this.txt_prezzo.TabIndex = 2;
+            this.txt_prezzo.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // Articolo
             // 
@@ -75,16 +79,46 @@
             this.btn_invia.UseVisualStyleBackColor = true;
             this.btn_invia.Click += new System.EventHandler(this.btn_invia_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Codice:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Descrizione:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(47, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Prezzo:";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btn_invia);
             this.Controls.Add(this.Articolo);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txt_prezzo);
+            this.Controls.Add(this.txt_descr);
+            this.Controls.Add(this.txt_cod);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -94,11 +128,14 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox txt_cod;
+        private System.Windows.Forms.TextBox txt_descr;
+        private System.Windows.Forms.TextBox txt_prezzo;
         private System.Windows.Forms.Label Articolo;
         private System.Windows.Forms.Button btn_invia;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- 10Articoli/Form1.cs
+++ 10Articoli/Form1.cs
@@ -19,8 +19,49 @@ namespace _10Articoli
 
         private void btn_invia_Click(object sender, EventArgs e)
         {
+            Articoli articoli = new Articoli();
+            articoli.Codice=float.Parse(txt_cod.Text);
+            articoli.Descrizione = txt_descr.Text;
+            articoli.Prezzo = float.Parse(txt_prezzo.Text);
+        }
 
-      
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+    class Articoli
+    {
+        public float _codice;
+        public string _descrizione;
+        public float _prezzo;
+        public bool fedelta=false;
+        public float Codice
+        {
+            get { return _codice; }
+            set { _codice = value; }
+        }
+
+        public string Descrizione
+        {
+            get { return _descrizione; }
+            set { _descrizione = value; }
+        }
+        public float Prezzo
+        {
+            get { return _prezzo; }
+            set { _prezzo = value; }
+        }
+        public void sconto()
+        {
+            if(fedelta==true)
+            {
+                Prezzo = Prezzo / 100 * 5;
+            }
+            else
+            {
+
+            }
         }
     }
 }
