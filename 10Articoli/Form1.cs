d35c2d5b272f1d54122f8b559ddfabd74ee34cfc
ste46751 <stefanquarti060@gmail.com>
2024-02-26 09:58:38 +0100

    sistemare fedeltà e stampare scontrino

--- 10Articoli/Form1.cs
+++ 10Articoli/Form1.cs
@@ -23,6 +23,9 @@ namespace _10Articoli
             pnl_fresco.Visible = false;
             pnl_nonalim.Visible=false;
         }
+        List<ArticoliAlimentare> aliment = new List<ArticoliAlimentare>();
+        List<ArticoliFresco> freschi = new List<ArticoliFresco>();
+        List<ArticoliNonAlimentare> nonAliment = new List<ArticoliNonAlimentare>();
 
         private void btn_invia_Click(object sender, EventArgs e)
         {
@@ -84,6 +87,19 @@ namespace _10Articoli
             articoliAlimentare.Descrizione = txt_descr1.Text;
             articoliAlimentare.Prezzo = int.Parse(txt_prezzo1.Text);
             articoliAlimentare.Anno = int.Parse(txt_scad.Text);
+
+            articoliAlimentare.sconto();
+
+            aliment.Add(articoliAlimentare);
+
+
+            pnl_nonalim.Visible = false;
+            pnl_alim.Visible = false;
+            pnl_fresco.Visible = false;
+            bttn_fresco.Visible = true;
+            bttn_nonalim.Visible = true;
+            bttn_alim.Visible = true;
+
         }
 
         private void bttn_salvafresco_Click(object sender, EventArgs e)
@@ -93,6 +109,17 @@ namespace _10Articoli
             articolifresco.Descrizione = txt_descr2.Text;
             articolifresco.Prezzo = int.Parse(txt_prez2.Text);
             articolifresco.Giorni = int.Parse(txt_scad.Text);
+
+            articolifresco.sconto();
+
+            freschi.Add(articolifresco);
+
+            pnl_nonalim.Visible = false;
+            pnl_alim.Visible = false;
+            pnl_fresco.Visible = false;
+            bttn_fresco.Visible = true;
+            bttn_nonalim.Visible = true;
+            bttn_alim.Visible = true;
         }
 
         private void bttn_salvaNoNalim_Click(object sender, EventArgs e)
@@ -102,8 +129,29 @@ namespace _10Articoli
             articoliNonAlimentare.Descrizione = txt_descr3.Text;
             articoliNonAlimentare.Prezzo = int.Parse(txt_prez3.Text);
             articoliNonAlimentare.Materiale = txt_mater.Text;
+            if(checkSi.Checked.Equals(true) && checkNo.Checked.Equals(false))
+            {
+                articoliNonAlimentare.Reciclabile = true;
+            }
+            else if (checkSi.Checked.Equals(false) && checkNo.Checked.Equals(true))
+            {
+                articoliNonAlimentare.Reciclabile = false;
+            }
+            else
+            { }
+
+            articoliNonAlimentare.sconto();
+
+            nonAliment.Add(articoliNonAlimentare);
+
+            pnl_nonalim.Visible = false;
+            pnl_alim.Visible = false;
+            pnl_fresco.Visible = false;
+            bttn_fresco.Visible = true;
+            bttn_nonalim.Visible = true;
+            bttn_alim.Visible = true;
         }
-    }
+    } 
     class Articoli
     {
         public string _codice;
@@ -152,7 +200,7 @@ namespace _10Articoli
         {
             if (Anno == DateTime.Today.Year)
             {
-                Prezzo = Prezzo / 100 * 20;
+                Prezzo -= Prezzo / 100 * 20;
             }
             else
             {
@@ -175,9 +223,9 @@ namespace _10Articoli
         }
         public override void sconto()
         {
-            if (_reciclabile == true)
+            if (Reciclabile == true)
             {
-                Prezzo = Prezzo / 100 * 10;
+                Prezzo -= Prezzo / 100 * 10;
             }
             else
             {
@@ -195,14 +243,12 @@ namespace _10Articoli
         }
         public override void sconto()
         {
-            if (fedelta == true)
+            int sco=0;
+            for (int i=0;i<Giorni; i++) 
             {
-                Prezzo = Prezzo / 100 * 5;
+                sco = 10 - 2 * i;
             }
-            else
-            {
-
-            }
+            Prezzo -= Prezzo / 100 * sco;
         }
     }
 }
